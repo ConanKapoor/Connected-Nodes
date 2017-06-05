921e0d197b32ee2494de087bffdfa799610895a2
ConanKapoor <kapoor.shivam88@gmail.com>
2017-06-05 19:32:10 +0530

    Comments Added for Readbility.

--- ConnectedNodes.py
+++ ConnectedNodes.py
@@ -1,28 +1,36 @@
 #Author - Shivam Kapoor
 
+#Importing essentials.
 import os
 import threading
 
+#Function to scan all nodes from "subnet.1" to "subnet.254".
 def find_nodes(ip,start,end):
     subnet=extract_subnet(ip)
     for node in xrange(start,end):
         IP=subnet+str(node)
+
+	#Threading Implementation Ahead - 
         thread=threading.Thread(target=find_Alive_Nodes,args=(IP,))
         thread.start()
 
+#Function to extract subnet and return xxx.xxx.xxx.
 def extract_subnet(ip):
     subnet=ip.split('.')
     dot='.'
     subnet=subnet[0]+dot+subnet[1]+dot+subnet[2]+dot
     return subnet
 
+#Function to open terminal and execute ping command.
 def find_Alive_Nodes(ip):
     execute="ping -c 1"+" "+ip
     result=os.popen(execute)
     for p in result.readlines():
         if p.count('ttl'):
+	    #Printing Alive Nodes.
             print ("[+] Node Found Alive--------->[%s]" %ip)
 
+#This is called first(Duh!)
 if __name__=='__main__':
     IP=str(raw_input("Enter the Network Id: "))
     try:
