643e035fd29c7f2b4c4555ca37832b5109343c7a
ConanKapoor <kapoor.shivam88@gmail.com>
2017-06-05 19:18:34 +0530

    Added Subnet Extraction

--- ConnectedNotes.py
+++ ConnectedNotes.py
@@ -1,5 +1,15 @@
 #Author - Shivam Kapoor
 
+def network_subnet(ip):
+    subnet=ip.split('.')
+    dot='.'
+    subnet=subnet[0]+dot+subnet[1]+dot+subnet[2]+dot
+    return subnet
+
 if __name__=='__main__':
     IP=str(raw_input("Enter the Network Id: "))
-
+    try:
+        network_subnet(IP)
+	print (subnet)
+    except:
+        print ("\nGive Network Id As: \n10.0.0.1")
